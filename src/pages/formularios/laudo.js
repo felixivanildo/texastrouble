42b5cff3642253a574031849407f36a7804f7b2d
Ivan Felix <felixsilva104@gmail.com>
2023-11-14 11:05:41 -0300

    holliday

--- src/pages/formularios/laudo.js
+++ src/pages/formularios/laudo.js
@@ -2,58 +2,96 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useForm, Controller } from 'react-hook-form';
 import { Button, TextField, Select, MenuItem } from "@mui/material";
-import { Throw } from "src/components/functions/Rdminethrow";
+import { CloudUpload, TipsAndUpdatesOutlined } from "@mui/icons-material";
+import { Tipo } from "./tiposLaudo/aguaTipos";
+//import { Throw } from "src/components/functions/Rdminethrow";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+// import './Laudo.css'
+// import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { VisuallyHiddenInput } from "@chakra-ui/visually-hidden";
 
 export default function Laudo() {
     const { handleSubmit, control } = useForm();
     const [selectedImage, setSelectedImage] = useState([]);
     const [sectors, setSectors] = useState([{}])
-    const [currentuser, setCurrentUser] = useState ([{}])
+    const [currentuser, setCurrentUser] = useState([{}])
+    const [selectedName, setSelectedName] = useState("");
+    const [fotoMensagem, setFotoMensagem] = useState("");
 
     const onSubmit = (data) => {
-        
-        Throw(currentuser,data,selectedImage)
-        // Handle form submission logic here
+
+        console.log(data)
+        // Throw(currentuser,data,selectedImage)
     };
+
+    const handleSelect = (data) => {
+        // console.log(data)
+
+        Tipo({"agua": "agua_bruta", "report": data}).then((e)=>{
+            // console.log(typeof(e))
+            setJson(e)
+        })
+    }
 
     const handleImageChange = (event) => {
         const files = event.target.files;
         const imagesArray = [];
-    
+        const file = event.target.files[0];
+        setSelectedName(file.name);
+
         for (let i = 0; i < files.length; i++) {
             const reader = new FileReader();
-    
+
             reader.onload = (e) => {
                 imagesArray.push({
                     "imagecode": e.target.result,
                     "extension": files[i].name.substring(files[i].name.length - 3),
                     "name": files[i].name.substring(0, files[i].name.length - 3),
                 });
-    
+
                 if (imagesArray.length === files.length) {
                     setSelectedImage(imagesArray);
                 }
+                if (files.length >= 2) {
+                    setSelectedName(`${files.length} arquivos escolhidos`)
+                }
             };
-    
+
             reader.readAsDataURL(files[i]);
+
+
+            if (files.length <= 0) {
+                setFotoMensagem("");
+            }
+            else if (files.length === 1) {
+                setFotoMensagem(`${files.length} Arquivos Selecionados`);
+            }
+            else {
+                setFotoMensagem(`${files.length} Arquivos Selecionados`);
+            }
         }
 
-       
     };
 
-    useEffect(()=>{
-        const autoexec = async ()=>{
-        const elaborate = await axios.get('http://10.254.4.132:3010/api/sectors')
-        const user = await AsyncStorage.getItem('@user')
-        setSectors(elaborate.data)
-        setCurrentUser(JSON.parse(user))
+    useEffect(() => {
+        const autoexec = async () => {
+            const elaborate = await axios.get('http://10.254.4.132:3010/api/sectors')
+            const user = await AsyncStorage.getItem('@user')
+            setSectors(elaborate.data)
+            setCurrentUser(JSON.parse(user))
         }
 
         autoexec()
     }, [])
 
-    const json = [
+    const [tipos, setTipos] = useState ([
+        {"tipo": "fisico_quimico", "name":"FÍSICO QUIMICO"},
+        {"tipo": "bacteriologica", "name":"BACTERIOLOGICA"},
+        {"tipo": "clorofila", "name": "CLOROFILA"}
+    
+    ])
+
+    const [json, setJson] = useState ( [
         { "name": "ph", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
         { "name": "corverdadeira", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
         { "name": "turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
@@ -81,111 +119,147 @@ export default function Laudo() {
         { "name": "escherichiacoli", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
         { "name": "indice_nitrato_nitrito", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
 
-    ]
+    ]) 
+    
     return (
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)", gap: "30px" }}>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                <Controller
-                            name={'reportname'}
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    style={{marginLeft: "30%"}}
-                                    id="outlined-static"
-                                    label="Nome do Laudo"
-                                    size="small"
-                                    
-                                    {...field}
-                                />
-                            )}
-                        />
 
-                        <br></br>
-                    <div className="mainform">
-                        {/* Loop through columns in JSON and create form fields */}
-                        {json.map((column) => (
-                            <div className="item" key={column.name}>
-                                {/* Use Controller to integrate React Hook Form with input fields */}
-                                <Controller
-                                    name={column.name}
-                                    control={control}
-                                    defaultValue=""
-                                    render={({ field }) => (
-                                        <TextField
-                                            id="outlined-basic"
-                                            label={column.name}
-                                            variant="outlined"
-                                            size="small"
-                                            type="text"
-                                            {...field}
+        <div>
 
-                                        />
-                                    )}
-                                />
-                            </div>
-                        ))}
+            <div>
+                <Button style={{backgroundColor:`wheat`}}>ÁGUA BRUTA</Button>
+                <Button>ÁGUA TRATADA</Button>
 
-                        <div className="item">
-                            <Controller
-                                name={'sector'}
-                                control={control}
-                                defaultValue=""
-                                render={({ field }) => (
-                                    <Select
-                                        style={{ minWidth: "120px" }}
-                                        placeholder="Setor"
-                                        id="demo-simple-select"
-                                        size="small"
-                                        defaultValue="Setor"
-                                        {...field}
-                                    >
-                                        {sectors.map((sec)=> (
-                                            <MenuItem value={sec.sectoraka}>{sec.sectoraka}</MenuItem>
-                                            ))}
-                                    </Select>
-                                )}
-                            />
-
-                        </div>
-                        <Controller
-                            name={'notes'}
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    className="item"
-                                    id="outlined-multiline-static"
-                                    label="Nota"
-                                    multiline
-                                    rows={4}
-                                    defaultValue="Default Value"
-                                    {...field}
-                                />
-                            )}
-                        />
-
-
-                    </div>
-
-
-
-                    <input onChange={handleImageChange} type="file" placeholder="Anexar Fotos" multiple  ></input>
-
-                    <br />
-
-
-
-                    <Button style={{ marginTop: "10px" }} type="submit" variant="contained" color="primary">
-                        Submit
-                    </Button>
-
-                </form>
+                {tipos.map((column) => (
+                 <div key={column.tipo}>
+                 <Button onClick={()=>{handleSelect(column.tipo)}}>{column.name}</Button>
+                 </div>
+                ))}
 
             </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
 
+                <div>
+                    <Controller
+                        name={'reportname'}
+                        control={control}
+                        defaultValue="" 
+                        render={({ field }) => (
+                            <TextField
+                                variant="outlined" 
+                                InputProps={{ sx: { borderRadius: "10px" } }}
+                                style={{ minWidth: "100%", marginTop: "15px" }}
+                                id="outlined-static" 
+                                label="Nome do Laudo" 
+                                size="small" 
+                                {...field}
+                            />
+                        )}
+                    /></div>
 
+                <br></br>
+                <div className="mainform">
+                    {/* Loop through columns in JSON and create form fields */}
+                    {json.map((column) => (
+                        <div key={column.name}>
+                            {/* Use Controller to integrate React Hook Form with input fields */}
+
+                            <Controller
+                                name={column.name}
+                                control={control}
+                                defaultValue="" 
+                                render={({ field }) => (
+                                    <TextField
+                                        InputProps={{ sx: { borderRadius: "10px" } }}
+                                        style={{ minWidth: "100%" }}
+                                        id="outlined-basic" 
+                                        label={column.name}
+                                        variant="outlined" 
+                                        size="small" 
+                                        type="text" 
+                                        {...field}
+
+                                    />
+                                )}
+                            />
+                        </div>
+                    ))}
+
+                        <Controller
+                            name={'sector'}
+                            control={control}
+                            defaultValue="" 
+                            render={({ field }) => (
+                                <Select
+                                    sx={{ borderRadius: "10px" }}
+                                    placeholder="Setor" 
+                                    id="demo-simple-select" 
+                                    size="small" 
+                                    defaultValue="Setor" 
+                                    {...field}
+                                >
+                                    {sectors.map((sec) => (
+                                        <MenuItem value={sec.sectoraka}>{sec.sectoraka}</MenuItem>
+                                    ))}
+                                </Select>
+                            )}
+                        />
+
+                    <Controller
+                        name={'notes'}
+                        control={control}
+                        defaultValue="" 
+                        render={({ field }) => (
+                            <TextField
+                                InputProps={{ sx: { borderRadius: "10px" } }}
+                                className="item-select" 
+                                id="outlined-multiline-static" 
+                                label="Nota" 
+                                multiline
+                                rows={4}
+                                defaultValue="Default Value" 
+                                {...field}
+                            />
+                        )}
+                    />
+                </div>
+
+                  <div style={{display: "flex", alignItems: "center", padding: "20px", marginBottom: "20px", justifyContent: "space-evenly", flexDirection:"column"}}>
+                        <Button
+                            size="small" 
+                            color="secondary" 
+                            component="label" 
+                            variant="contained" 
+                            startIcon={<CloudUpload />}
+                            style={{
+                                marginTop: "10px",
+                                position: "relative",
+                            }}
+                        >
+                            Anexar Fotos
+                            <VisuallyHiddenInput
+                                multiple
+                                type="file" 
+                                onChange={handleImageChange}
+                            />
+                        </Button>
+
+                        <div style={{display:"flex", justifyContent:"center", paddingBottom:"20px"}}>
+                        <p style={{fontSize: 16, }}>{fotoMensagem}</p>
+                        </div>
+                  </div>
+
+                  
+
+                    <Button
+                        style={{ marginTop: "20px", padding: 10 }}
+                        fullWidth
+                        type="submit" 
+                        color="primary" 
+                        variant="contained" 
+                    >
+                        Submit
+                    </Button>
+            </form>
         </div>
     );
 }
---
+++ src/pages/formularios/tiposLaudo/aguaTipos.js
@@ -0,0 +1,149 @@
+
+
+export async function Tipo (data){
+
+    // console.log(data.report)
+
+    if(data.agua= "agua_bruta"){
+        
+            if(data.report=== "fisico_quimico"){
+                return [{ "name": "ph" },
+                { "name": "cor_aparente" },
+                { "name": "turbidez" },
+                { "name": "condutancia_especifica" },
+                { "name": "acidez" },
+                { "name": "alcalinidade_oh" },
+                { "name": "alcalinidade_co" },
+                { "name": "alcalinidade_hco" },
+                { "name": "dureza_total" },
+                { "name": "dureza_carbonatos" },
+                { "name": "dureza_ncarbonatos" },
+                { "name": "calcio" },
+                { "name": "magnesio" },
+                { "name": "cloretos" },
+                { "name": "silica" },
+                { "name": "sulfato" },
+                { "name": "amonia" },
+                { "name": "nitrato" },
+                { "name": "nitrito" },
+                { "name": "indice_nitrato_nitrito" },
+                { "name": "ferro_total" },
+                { "name": "sodio" },
+                { "name": "potassio" },
+                { "name": "solidos_totais" },
+                { "name": "cloro_residual_livre" },
+                { "name": "coliformes_totais" },
+                { "name": "escherichia_coli" }]
+            }
+            if(data.report==="bacteriologica"){
+                // console.log('entrei')
+                return [
+                    { "name": "nmr_amostra" },
+                    { "name": "data_coleta" },
+                    { "name": "local_coleta" },
+                    { "name": "cor" },
+                    { "name": "turbidez" },
+                    { "name": "cloro_residual_livre" },
+                    { "name": "coliformes_totais" },
+                    { "name": "escherichia_coli" },
+                    { "name": "ph" }
+                ]
+            }
+
+
+
+            if(data.report==="clorofila"){
+                return [{ "name": "procedencia" },
+                { "name": "endereco" },
+                { "name": "data_coleta" },
+                { "name": "hora_coleta" },
+                { "name": "observacao-do-ambiente" },
+                { "name": "entrada-no-laboratorio" },
+                { "name": "condicao-da-amostra" },
+                { "name": "inicio-analise" },
+                { "name": "termino-analise" },
+                { "name": "volume-filtrado" },
+                { "name": "metodo" },
+                { "name": "resultado" }]
+            }
+    }
+
+
+    if(data.agua="Água Tratada"){
+        if(data.report="fisico_quimico"){
+            return [ { "name": "ph", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "cor_verdadeira", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "condutancia_especifica", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "acidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "alcalinidade_oh", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "alcalinidade_co", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "alcalinidade_hco", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "dureza_total", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "dureza_carbonatos", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "dureza_ncarbonatos", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "calcio", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "magnesio", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "cloretos", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "silica", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "sulfato", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "amonia", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "nitrato", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "nitrito", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "ferro_total", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "sodio", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "potassio", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "solidos_totais", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "coliformes_totais", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "escherichia_coli", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+            { "name": "clorofila_a", "text": "character varying(15) COLLATE pg_catalog.\"default\"" }]
+        }
+
+
+        if(data.report="bacteriologica"){
+            return [
+                { "name": "numero_amostra", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "data_coleta", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "local_coleta", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "cor", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "cloro_residual_livre", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "coliformes_totais", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "escherichia_coli", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "ph", "text": "character varying(15) COLLATE pg_catalog.\"default\"" }
+            ]
+        }
+
+
+
+        if(data.report="iqa_eta_poco"){
+            return [
+                { "name": "data_coleta", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "ponto_coleta", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "cor", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "cloro", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "coliformeis", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "e_coli", "text": "character varying(15) COLLATE pg_catalog.\"default\"" }
+            ]
+        }
+
+
+
+        if(data.report="snis"){
+            return [
+                { "name": "municipio", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_minima_amostras_cloro_residual", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_cloro_residual", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_cloro_residual_fora_do_padrao", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_minima_amostras_turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_turbidez", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_turbidez_fora_do_padrao", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_minima_amostras_coliformes_totais", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_coliformes_totais", "text": "character varying(15) COLLATE pg_catalog.\"default\"" },
+                { "name": "quantidade_amostras_coliformes_totais_fora_do_padrao", "text": "character varying(15) COLLATE pg_catalog.\"default\"" }
+            ]
+        }
+    }
+   
+}
